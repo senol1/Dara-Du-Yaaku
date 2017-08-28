62b7bdd1e734a53821dff137dfa479b3f0de2181
Mame Medoune Diop <m3doune@gmail.com>
2017-08-28 10:55:22 +0000

    Ajout googl autocomplete

--- src/AppBundle/Resources/public/js/search.js
+++ src/AppBundle/Resources/public/js/search.js
@@ -27,6 +27,7 @@ L.Control.Search = L.Control.extend({
     initialize: function (options /*{ data: {...}  }*/) {
         // constructor
         L.Util.setOptions(this, options);
+        this.loadGoogleApi();
     },
     setSearchData: function (data) {
         this.options.data = data;
@@ -38,10 +39,12 @@ L.Control.Search = L.Control.extend({
         container.style.cssText = 'max-width: 250px; min-width: 250px; ';
         this.form = L.DomUtil.create('form', 'form', container);
         var group = L.DomUtil.create('div', 'form-group', this.form);
-        this.input = L.DomUtil.create('input', '', group);
+        this.input = L.DomUtil.create('input', 'ddy_search_place', group);
         this.input.type = 'search';
         this.input.placeholder = this.options.placeholder;
         this.results = L.DomUtil.create('div', 'content list', group);
+        var shopList = L.DomUtil.create('div', 'list-group', container);
+        shopList.style.cssText = 'min-height: 350px; background-color: rgba(236, 240, 241,0.85);';
         L.DomEvent.addListener(this.input, 'keyup', _.debounce(this.keyup, 300), this);
         L.DomEvent.addListener(this.input, 'focusout', function (e) {
             var elem = e.target
@@ -63,32 +66,7 @@ L.Control.Search = L.Control.extend({
         L.DomEvent.removeListener(form, 'submit', this.submit, this);
     },
     keyup: function (e) {
-        console.log(e.target.value);
-        // var that = this;
-        // var fuse = new Fuse(this.options.data, this.fuse_options);
-        // if (e.keyCode === 38 || e.keyCode === 40) {
-        //     // do nothing
-        // } else {
-        //     var searchResults = this.results;
-        //     searchResults.innerHTML = '';
-        //     if (this.input.value.length > 2) {
-        //
-        //         var value = this.input.value;
-        //         var results = fuse.search(value);
-        //         _.map(results, function (x) {
-        //             var a = L.DomUtil.create('a', 'list-item');
-        //             var text = L.DomUtil.create('div', 'list-text', a);
-        //             // a.href = '#';
-        //             a.setAttribute('data-result-name', x.item.properties.nom);
-        //             text.innerHTML = x.item.properties.nom;
-        //             // a.onclick = that.itemSelected;
-        //             searchResults.appendChild(a);
-        //             L.DomEvent.addListener(a, 'click', this.itemSelected, this);
-        //
-        //             return a;
-        //         }, this);
-        //     }
-        // }
+        // console.log(e.target.value);
     },
     itemSelected: function (e) {
         L.DomEvent.preventDefault(e);
@@ -107,6 +85,32 @@ L.Control.Search = L.Control.extend({
     },
     submit: function (e) {
         L.DomEvent.preventDefault(e);
+    },
+    loadGoogleApi: function () {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyAr-Ecd4ShkBlVGNw2y7JzmrV_mByqv_RI';
+        // if (callback){
+        //     script.onreadystatechange = callback;
+        //     script.onload = script.onreadystatechange;
+        // }
+
+        script.onload = function () {
+            console.log(this);
+            var options = {
+                types: ['(regions)'],
+                componentRestrictions: {country: 'sn'}
+            };
+
+            var searchBox = document.getElementsByClassName('ddy_search_place')[0];
+
+            console.log(searchBox);
+
+
+            autocomplete = new google.maps.places.Autocomplete(searchBox, options);
+        };
+
+        document.body.appendChild(script);
     }
 
 });
